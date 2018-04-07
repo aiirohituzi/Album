987e0ff32a87d556ad9dd3cce00621be33f514a9
DESKTOP-05FLCR5\sinn5 <sinn5656@gmail.com>
2018-04-07 23:29:27 +0900

    Create base view.

--- Album_server/photo/views.py
+++ Album_server/photo/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from photo.models import photo
+import json
+from django.db.models import Q
 
 # Create your views here.
+def getPhoto(request):
+    data = []
+    
+    for item in photo.objects.all():
+        data.append({
+            'id': item.id,
+            'title': item.title,
+            'content': item.content,
+            'path': item.path,
+            'created_at': item.created_at,
+        })
+
+    print("Get - Photo")
+    data = json.dumps(data, indent=4)
+    print(data)
+    
+    return HttpResponse(data, content_type = "application/json")
