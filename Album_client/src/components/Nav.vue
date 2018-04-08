a831cc3ff01ca30ad0f245d15af53c7b91ed09f8
DESKTOP-05FLCR5\sinn5 <sinn5656@gmail.com>
2018-04-08 19:39:25 +0900

    Create side navigation frame.

---
+++ Album_client/src/components/Nav.vue
@@ -0,0 +1,43 @@
+<template>
+<ul id="navigation">
+   <li><a>1</a></li>
+   <li><a>2</a></li>
+   <li><a>3</a></li>
+   <li><a>4</a></li>
+   <li><a>5</a></li>
+</ul>
+</template>
+
+<script>
+</script>
+
+<style>
+ul#navigation {
+    position: fixed;
+    margin: 0px;
+    padding: 0px;
+    top: 0px;
+    left: 0px;
+    width: 300px;
+    height: 100%;
+    list-style: none;
+    z-index:9999;
+}
+ul#navigation li {
+    width: 100%;
+    height: 20%;
+}
+ul#navigation li a {
+    display: block;
+    margin-left: 0px;
+    width: 100%;
+    height: 99%;
+    background-color:#CFCFCF;
+    background-repeat:no-repeat;
+    background-position:center center;
+    border:1px solid #AFAFAF;
+    -moz-box-shadow: 0px 2px 3px #333;
+    -webkit-box-shadow: 0px 2px 3px #333;
+    box-shadow: 0px 2px 3px #333;
+}        
+</style>
--- Album_client/src/router/index.js
+++ Album_client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Nav from '@/components/Nav'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Nav',
+      name: 'Nav',
+      component: Nav
     }
   ]
 })
