7ce783f57b860b3f6e41359e6c523769ba18a4d6
DESKTOP-05FLCR5\sinn5 <sinn5656@gmail.com>
2018-04-11 23:20:09 +0900

    Add side navigation close button.

--- Album_client/src/App.vue
+++ Album_client/src/App.vue
@@ -1,16 +1,17 @@
 <template>
     <div class="container">
         <ul id="navigation" class="side">
+            <li><a @click="navClose()"><div class="closeBtn">X</div></a></li>
             <router-link tag="li" to="/" exact><a>Home(임시)</a></router-link>
             <router-link tag="li" to="/Photo" exact><a>Photo(임시)</a></router-link>
             <li><a>3</a></li>
             <li><a>4</a></li>
             <li><a>5</a></li>
         </ul>
-        <div class="circle" @click="move()">
-
+        <div class="navBtn" @click="navOpen()"></div>
+        <div class="main">
+            <router-view/>
         </div>
-        <router-view/>
     </div>
 </template>
 
@@ -18,48 +19,47 @@
 export default {
     name: 'App',
     methods: {
-        move: function () {
-            var side = document.querySelector('.side');
-            side.classList.toggle('move');
+        navOpen: function () {
+            var side = document.querySelector('.side')
+            var navBtn = document.querySelector('.navBtn')
+            var main = document.querySelector('.main')
+            side.classList.toggle('move')
+            navBtn.classList.toggle('hidden')
+            main.classList.toggle('move')
         },
+        navClose: function () {
+            var side = document.querySelector('.side')
+            var navBtn = document.querySelector('.navBtn')
+            var main = document.querySelector('.main')
+            side.classList.toggle('move')
+            navBtn.classList.toggle('hidden')
+            main.classList.toggle('move')
+        }
     }
 }
 </script>
 
 <style>
 .container {
-    margin-top: 15px;
-    margin-left: 30px;
+    margin-top: 20px;
+    margin-left: 15px;
 }
+
 ul#navigation {
     position: fixed;
     margin: 0px;
     padding: 0px;
     top: 0px;
-    left: -300px;
+    left: -305px;
     width: 300px;
     height: 100%;
     list-style: none;
     z-index:9999;
     
     background-color:#CFCFCF;
-    /* border:1px solid #AFAFAF; */
-    /* -moz-box-shadow: 0px 2px 3px #333;
-    -webkit-box-shadow: 0px 2px 3px #333;
-    box-shadow: 0px 2px 3px #333; */
-}
-.side {
-    -webkit-transform: translate(0, 0);
-    -webkit-transition: -webkit-transform 500ms;
-
-    transform: translate(0, 0);
-    transition: transform 500ms;
-
-    will-change: transform;
-}
-.side.move {
-    -webkit-transform: translate(300px, 0px);
-    transform: translate(300px, 0px);
+    -moz-box-shadow: 0px 4px 6px #333;
+    -webkit-box-shadow: 0px 4px 6px #333;
+    box-shadow: 0px 4px 6px #333;
 }
 ul#navigation li {
     width: 100%;
@@ -75,7 +75,22 @@ ul#navigation li a {
     background-repeat:no-repeat;
     background-position:center center;
 }
-.circle {
+
+.side {
+    -webkit-transform: translate(0, 0);
+    -webkit-transition: -webkit-transform 500ms;
+
+    transform: translate(0, 0);
+    transition: transform 500ms;
+
+    will-change: transform;
+}
+.side.move {
+    -webkit-transform: translate(305px, 0px);
+    transform: translate(305px, 0px);
+}
+
+.navBtn {
     position: fixed;
     right: 30px;
     bottom: 30px;
@@ -86,5 +101,22 @@ ul#navigation li a {
     background-repeat:no-repeat;
     background-position:center center;
     background-image: url(assets/menu.png);
+}
+.navBtn.hidden {
+    visibility: hidden;
+}
+.closeBtn {
+    display: inline-block;
+    margin-right: 0px;
+    float: right;
+    text-align: center;
+    width: 50px;
+    height: 95%;
+    border:1px solid #AFAFAF;
+    background-repeat:no-repeat;
+    background-position:center center;
+}
+.main.move {
+    margin-left: 300px;
 }
 </style>
