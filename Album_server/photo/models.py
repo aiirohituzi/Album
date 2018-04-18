470d6cd7efe2e58d765af1286656e200c183af1d
DESKTOP-05FLCR5\sinn5 <sinn5656@gmail.com>
2018-04-18 16:51:51 +0900

    Install django-smartfields. Set image storage path to client-side directory.

--- Album_server/photo/models.py
+++ Album_server/photo/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from smartfields import fields
+
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 class photo(models.Model):
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=200)
     path = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Images(models.Model):
+    fs = FileSystemStorage(location=settings.FILES_DIR)
+    image = fields.ImageField(upload_to='%Y/%m/%d/orig', storage=fs)
+    created_at = models.DateTimeField(auto_now_add=True)
+    postId = models.ForeignKey(Posting, related_name='imageRelate', default='0')
+
+    class Meta:
+        ordering = ('-created_at', '-pk', )
