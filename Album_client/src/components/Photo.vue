1c3afd15039aa37ac2f02e186f505637e1a2108b
DESKTOP-05FLCR5\sinn5 <sinn5656@gmail.com>
2018-05-21 23:49:31 +0900

    Creating function to resize image while maintaining ratio.

--- Album_client/src/components/Photo.vue
+++ Album_client/src/components/Photo.vue
@@ -24,8 +24,8 @@
             </div>
             <div class="modal-content">
                 <div class="img-wrapper" v-if="!updateData.state_update">
-                    <img v-for="image in images" v-if="image.photoId == modal.photoId" :src="imagePath(image.image)" /><br>
-                    {{ this.modal.content }}
+                    <img v-for="image in images" v-if="image.photoId == modal.photoId" :src="imagePath(image.image)" />
+                    <br>{{ this.modal.content }}
                 </div>
 
                 <div v-else>
@@ -140,6 +140,7 @@ export default {
             modal.classList.toggle('toggle')
 
             if(id){
+                this.imgResize()
                 this.modal.photoId = id
                 for(var i=0; i<this.photos.length; i++){
                     if(this.photos[i].id == id) {
@@ -352,6 +353,28 @@ export default {
                     })
                 }
             }
+        },
+        imgResize: function() {
+            var div = document.querySelector('body > div > div.main > div > div.modal.modal-photo.toggle > div.modal-box > div.modal-content > div.img-wrapper');
+            console.log(div)
+            var divAspect = div.offsetHeight / div.offsetWidth;
+            div.style.overflow = 'hidden';
+            
+            var img = div.querySelector('img');
+            console.log(img)
+            var imgAspect = img.height / img.width;
+
+            if (imgAspect <= divAspect) {
+                // 이미지가 div보다 납작한 경우 세로를 div에 맞추고 가로는 잘라낸다
+                var imgWidthActual = div.offsetHeight / imgAspect;
+                var imgWidthToBe = div.offsetHeight / divAspect;
+                var marginLeft = -Math.round((imgWidthActual - imgWidthToBe) / 2)
+                img.style.cssText = 'width: auto; height: 100%; margin-left: '
+                                + marginLeft + 'px;'
+            } else {
+                // 이미지가 div보다 길쭉한 경우 가로를 div에 맞추고 세로를 잘라낸다
+                img.style.cssText = 'width: 100%; height: auto; margin-left: 0;';
+            }
         }
     },
     mounted: function () {
@@ -512,12 +535,13 @@ export default {
 .modal .modal-box .modal-content .img-wrapper {
     margin-left: auto;
     margin-right: auto;
-    max-width: 55vw;
-    max-height: 80vh;
+    width: 56vw;
+    height: 80vh;
+    overflow: hidden;
 }
 .modal .modal-box .modal-content .img-wrapper img {
-    max-width: 22vw;
-    max-height: 40vh;
+    max-width: 27vw;
+    max-height: 39vh;
     border: 1px solid #dddddd;
 }
 .modal .modal-box .modal-bottom {
