d62e39f6c47969c06cc5b8cf609677ff2058fa5f
DESKTOP-05FLCR5\sinn5 <sinn5656@gmail.com>
2018-05-22 23:36:57 +0900

    Add detail image modal.

--- Album_client/src/components/Photo.vue
+++ Album_client/src/components/Photo.vue
@@ -69,6 +69,14 @@
         </div>
     </div>
 
+    <div class="modal modal-detailImg">
+        <div class="modal-background" @click="modalToggle('')">
+        </div>
+        <div class="modal-box">
+            <div class="modal-content">
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
@@ -140,7 +148,6 @@ export default {
             modal.classList.toggle('toggle')
 
             if(id){
-                this.imgResize()
                 this.modal.photoId = id
                 for(var i=0; i<this.photos.length; i++){
                     if(this.photos[i].id == id) {
@@ -354,28 +361,6 @@ export default {
                 }
             }
         },
-        imgResize: function() {
-            var div = document.querySelector('body > div > div.main > div > div.modal.modal-photo.toggle > div.modal-box > div.modal-content > div.img-wrapper');
-            console.log(div)
-            var divAspect = div.offsetHeight / div.offsetWidth;
-            div.style.overflow = 'hidden';
-            
-            var img = div.querySelector('img');
-            console.log(img)
-            var imgAspect = img.height / img.width;
-
-            if (imgAspect <= divAspect) {
-                // 이미지가 div보다 납작한 경우 세로를 div에 맞추고 가로는 잘라낸다
-                var imgWidthActual = div.offsetHeight / imgAspect;
-                var imgWidthToBe = div.offsetHeight / divAspect;
-                var marginLeft = -Math.round((imgWidthActual - imgWidthToBe) / 2)
-                img.style.cssText = 'width: auto; height: 100%; margin-left: '
-                                + marginLeft + 'px;'
-            } else {
-                // 이미지가 div보다 길쭉한 경우 가로를 div에 맞추고 세로를 잘라낸다
-                img.style.cssText = 'width: 100%; height: auto; margin-left: 0;';
-            }
-        }
     },
     mounted: function () {
         this.fetchPhotos()
@@ -541,7 +526,7 @@ export default {
 }
 .modal .modal-box .modal-content .img-wrapper img {
     max-width: 27vw;
-    max-height: 39vh;
+    max-height: 38vh;
     border: 1px solid #dddddd;
 }
 .modal .modal-box .modal-bottom {
