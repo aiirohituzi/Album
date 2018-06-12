6cf4dcebf82e9c7226411adf4b049a509c5c39f4
DESKTOP-05FLCR5\sinn5 <sinn5656@gmail.com>
2018-06-12 23:21:43 +0900

    Change sign-in UI from variable size to fixed size.

--- Album_client/src/components/Sign.vue
+++ Album_client/src/components/Sign.vue
@@ -1,10 +1,12 @@
 <template>
 <div class="container-main">
     <div class="wrapper-sign">
+        <h2>관리자 계정 로그인</h2>
         <div class="input-group">
             <input type="text" placeholder="ID"><br>
-            <input type="text" placeholder="PW">
+            <input type="text" placeholder="PW"><br>
         </div>
+        <div class="sign-in">Sign in</div>
     </div>
 </div>
 </template>
@@ -18,20 +20,38 @@
     margin-left: 10px;
 }
 .wrapper-sign {
-    position: relative;
-    width: 30vw;
-    height: 50vh;
-    min-width: 200px;
-    min-height: 333px;
-    top: 15vh;
+    width: 300px;
+    height: 350px;
     text-align: center;
-    margin: auto;
+    margin-top: 20vh;
+    margin-left: auto;
+    margin-right: auto;
     border: 1px solid #111;
 }
+.wrapper-sign h2{
+    margin-top: 50px;
+    margin-bottom: 50px;
+}
 .wrapper-sign .input-group {
-    margin-top: 60%;
+    /* margin-top: 30vh; */
 }
 .wrapper-sign .input-group input{
     margin: 10px;
+    width: 200px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+}
+.wrapper-sign .sign-in{
+    margin-top: 10px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 200px;
+    background-color: #1175cc;
+    border-radius: 3px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+}
+.wrapper-sign .sign-in:hover{
+    background-color: #1165cc;
 }
 </style>
