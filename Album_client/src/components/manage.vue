c7df9e1807cd9b0bdfe19b0d9b8a8e28820d0848
DESKTOP-05FLCR5\sinn5 <sinn5656@gmail.com>
2018-06-27 23:54:57 +0900

    Add table CSS. Add a checkbox default action method.

--- Album_client/src/components/manage.vue
+++ Album_client/src/components/manage.vue
@@ -1,25 +1,24 @@
 <template>
 <div>
-    asdfasfd
     <button @click="signOut()">Sign out</button>
-    <table border="1">
+    <table class="photos">
         <tr>
             <th>글번호</th>
             <th>제목</th>
             <th>생성일</th>
-            <th>선택<input type="checkbox"></th>
+            <th><input type="checkbox" v-model="allChecked" @click="allCheck()"></th>
         </tr>
         <tr v-if="length==1">
             <td>{{ photos[0].id }}</td>
             <td>{{ photos[0].title }}</td>
-            <td>{{ photos[0].created }}</td>
-            <td><input type="checkbox"></td>
+            <td>{{ photos[0].created.split('.')[0] }}</td>
+            <td><input type="checkbox" v-model="photos[0].checked"></td>
         </tr>
         <tr v-else v-for="n in max">
             <td>{{ photos[n-1].id }}</td>
             <td>{{ photos[n-1].title }}</td>
-            <td>{{ photos[n-1].created }}</td>
-            <td><input type="checkbox"></td>
+            <td>{{ photos[n-1].created.split('.')[0] }}</td>
+            <td><input type="checkbox" v-model="photos[n-1].checked"></td>
         </tr>
     </table>
 </div>
@@ -38,11 +37,13 @@ export default {
                     'title': '',
                     'content': '',
                     'created': '',
+                    'checked': '',
                 },
             ],
             length: 1,
             max: 10,
             more: true,
+            allChecked: false,
         }
     },
     beforeCreate: function () {
@@ -71,10 +72,46 @@ export default {
         signOut: function () {
             this.$session.destroy()
             this.$router.push('/Sign')
+        },
+
+        allCheck: function () {
+            if(this.allChecked == false) {
+                for(var i=0; i<this.length; i++){
+                    this.photos[i].checked = true;
+                }
+            } else {
+                for(var i=0; i<this.length; i++){
+                    this.photos[i].checked = false;
+                }
+            }
         }
     },
 }
 </script>
 
 <style>
+.photos {
+    border-collapse: collapse;
+    margin-top: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 70%;
+    text-align: center;
+}
+.photos tr, td {
+    border-top: 2px solid #ccc;
+    padding: 10px;
+}
+
+@media only screen and (min-width: 768px) and (max-width: 1023px) {
+    .photos {
+        width: 80%;
+    }
+}
+@media only screen and (max-width: 767px) {
+    .photos {
+        width: 95%;
+        font-size: 9pt;
+    }
+}
 </style>
