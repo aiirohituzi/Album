ee73219e37484e64d29cf431a703888d04aecaac
DESKTOP-05FLCR5\sinn5 <sinn5656@gmail.com>
2018-06-28 23:48:58 +0900

    Add detail view function on the manage page.

--- Album_client/src/components/manage.vue
+++ Album_client/src/components/manage.vue
@@ -8,19 +8,22 @@
             <th>생성일</th>
             <th><input type="checkbox" v-model="allChecked" @click="allCheck()"></th>
         </tr>
-        <tr v-if="length==1">
+        <tr class="tbody" v-if="length==1" @click="detailPhoto(0)">
             <td>{{ photos[0].id }}</td>
             <td>{{ photos[0].title }}</td>
             <td>{{ photos[0].created.split('.')[0] }}</td>
             <td><input type="checkbox" v-model="photos[0].checked"></td>
         </tr>
-        <tr v-else v-for="n in max">
+        <tr class="tbody" v-else v-for="n in max" @click="detailPhoto(n-1)">
             <td>{{ photos[n-1].id }}</td>
             <td>{{ photos[n-1].title }}</td>
             <td>{{ photos[n-1].created.split('.')[0] }}</td>
             <td><input type="checkbox" v-model="photos[n-1].checked"></td>
         </tr>
     </table>
+    <div class="div-detail">
+        {{ detail.content }}
+    </div>
 </div>
 </template>
 
@@ -40,6 +43,9 @@ export default {
                     'checked': '',
                 },
             ],
+            detail: {
+                'content': '',
+            },
             length: 1,
             max: 10,
             more: true,
@@ -74,6 +80,10 @@ export default {
             this.$router.push('/Sign')
         },
 
+        detailPhoto: function (num) {
+            this.detail.content = this.photos[num].content
+        },
+
         allCheck: function () {
             if(this.allChecked == false) {
                 for(var i=0; i<this.length; i++){
@@ -97,20 +107,50 @@ export default {
     margin-right: auto;
     width: 70%;
     text-align: center;
+    
+    -moz-transition: all .5s ease-in-out;
+    -webkit-transition: all .5s ease-in-out;
+    transition: all .5s ease-in-out;
 }
 .photos tr, td {
     border-top: 2px solid #ccc;
     padding: 10px;
+}
+.photos .tbody:hover {
+    background-color: #eee
+}
+
+.div-detail {
+    width: 65%;
+    overflow-y: scroll;
+    max-height: 20vh;
+    border-radius: 5px;
+    padding: 10px;
+    margin-top: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 2px solid #ccc;
+    
+    -moz-transition: all .5s ease-in-out;
+    -webkit-transition: all .5s ease-in-out;
+    transition: all .5s ease-in-out;
 }
 
 @media only screen and (min-width: 768px) and (max-width: 1023px) {
     .photos {
         width: 80%;
     }
+    .div-detail {
+        width: 75%;
+    }
 }
 @media only screen and (max-width: 767px) {
     .photos {
         width: 95%;
+        font-size: 9pt;
+    }
+    .div-detail {
+        width: 90%;
         font-size: 9pt;
     }
 }
