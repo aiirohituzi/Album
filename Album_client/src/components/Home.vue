41914d542f70b03830ba9459c836742453c2a071
DESKTOP-05FLCR5\sinn5 <sinn5656@gmail.com>
2018-07-31 23:35:16 +0900

    Test to embed youtube from link.

--- Album_client/src/components/Home.vue
+++ Album_client/src/components/Home.vue
@@ -1,10 +1,36 @@
 <template>
 <div>
-<h1>Home</h1>
+    <h1>Home</h1>
+    
+    <iframe width="560" height="315"
+    frameborder="0"
+    :src="'//www.youtube.com/embed/' + getId('https://www.youtube.com/watch?v=4A-5GoOJ67k')">
+    </iframe>
+
 </div>
 </template>
 
 <script>
+export default {
+    name: 'Home',
+    data () {
+        return {
+            url: ''
+        }
+    },
+    methods: {
+        getId: function(url) {
+            var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+            var match = url.match(regExp);
+
+            if (match && match[2].length == 11) {
+                return match[2];
+            } else {
+                return 'error';
+            }
+        }
+    }
+}
 </script>
 
 <style>
