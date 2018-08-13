b1cc534d0fe3066c5da6316e411e1c0d81afbec7
DESKTOP-05FLCR5\sinn5 <sinn5656@gmail.com>
2018-08-13 23:44:25 +0900

    Practice one-page-scroll.

--- Album_client/src/components/Home.vue
+++ Album_client/src/components/Home.vue
@@ -1,10 +1,14 @@
 <template>
-<div>
+<div class="container-main">
+	<div class="item" id="item1">
     <h1>Home</h1>
     
     
     <div id="delta">Scroll mouse wheel to see delta here.</div>
-
+	</div>
+	<div class="item" id="item2">
+		page 2
+	</div>
 </div>
 </template>
 
@@ -13,9 +17,11 @@ function handle(delta) {
 	var s = delta + ": ";
 	if (delta < 0) {
 		s += "down";
+		scrollMove("item2");
     }
 	else {
 		s += "up";
+		scrollMove("item1");
     }
 	document.getElementById('delta').innerHTML = s;
 }
@@ -32,10 +38,37 @@ function wheel(event){
 		handle(delta);
 }
 
+	
+function getOffsetTop(el) {
+	var top = 0;
+	if(el.offsetParent){
+		do{
+			top += el.offsetTop
+		} while(el = el.offsetParent)
+		return [top-55]
+	}
+}
+
+function scrollMove(id){
+	if(id==0){
+		window.scroll({
+			top:0,
+			behavior: 'smooth'
+		})
+	} else {
+		window.scroll({
+			top: getOffsetTop(document.getElementById(id)),
+			behavior: 'smooth'
+		})
+	}
+}
+
 /* Initialization code. */
 if (window.addEventListener)
 	window.addEventListener('DOMMouseScroll', wheel, false);
 window.onmousewheel = document.onmousewheel = wheel;
+
+
 export default {
     name: 'Home',
     data () {
@@ -48,7 +81,14 @@ export default {
 </script>
 
 <style>
-div {
+body {
+	overflow: hidden;
+}
+.container-main {
     height: 200vh;
+    display: block;
+}
+.item {
+	height: 100vh;
 }
 </style>
