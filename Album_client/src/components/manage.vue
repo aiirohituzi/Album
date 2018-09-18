2da76725651b866f3866057b2ae20f6e8b161650
aiirohituzi <sinn5656@gmail.com>
2018-09-18 23:45:26 +0900

    Fix issue #5.

--- Album_client/src/components/manage.vue
+++ Album_client/src/components/manage.vue
@@ -225,21 +225,27 @@ export default {
                     div.removeChild(ytb)
                 }
 
-                var regExp = /(https?:\/\/www.youtube.com\/watch\?v=[^#\&\?\n]{11,11})/
-                var regExp2 = /(https:\/\/www.youtube.com\/watch\?v=)([^#\&\?]{11,11}).*/
+                var regExp = /(https?:\/\/www.youtube.com\/watch\?v=[^#\&\?\n]{11,11})|(https?:\/\/youtu.be\/[^#\&\?\n]{11,11})/
+                var regExp2 = /((https?:\/\/www.youtube.com\/watch\?v=)([^#\&\?]{11,11}))|((https?:\/\/youtu.be\/)([^#\&\?]{11,11}))/
                 var split_content = this.detail.content.split(regExp)
-                
                 
                 var div_ytb = document.createElement("div")
                 div_ytb.setAttribute("id", 'ytb')
                 for(var i=0; i<split_content.length; i++){
                     if(regExp.test(split_content[i])){
                         var match = split_content[i].match(regExp2)
+                        // console.log(match)
                         if(match){
+                            var id
+                            if(match[3]){
+                                id = match[3]
+                            } else if(match[6]){
+                                id = match[6]
+                            }
                             var iframe = document.createElement("iframe")
                             
                             iframe.setAttribute("frameBorder", 'no')
-                            iframe.setAttribute( "src", '//www.youtube.com/embed/' + match[2]);
+                            iframe.setAttribute( "src", '//www.youtube.com/embed/' + id);
                             div_ytb.appendChild(iframe)
 
                             div_ytb.insertAdjacentHTML('beforeend', "<font size='1' color='gray'>" + match[0] + '</font><br><br>')
